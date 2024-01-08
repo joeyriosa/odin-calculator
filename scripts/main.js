0cd9e1f6db03a8cb3c9e4d30ef117c59049818d7
Joey Riosa <66200907+joeyriosa@users.noreply.github.com>
2024-01-08 21:45:04 +0800

    Update code with initial operate function
    The result is also being displayed now.

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
-let number1;
-let number2;
-let operation;
+let calculatorInput = {
+    num1: undefined,
+    num2: undefined,
+    operator: undefined
+}
+
+let displayNumber;
+let isOperatorLastClick = false;
+let isEqualsLastClick = false;
 
 let display = document.querySelector('#display');
+let formula = document.querySelector('#formula');
 
 function add(a, b) {
     return a + b;
@@ -37,11 +44,61 @@ function operate(operator, num1, num2) {
 }
 
 function handleNumberClick(event) {
-    console.log(event.target.textContent);
-    display.textContent = event.target.textContent;
+    //console.log(event.target.textContent);
+    if ((calculatorInput.num1 === undefined && display.textContent === "0") || isOperatorLastClick === true) {
+        display.textContent = event.target.textContent;
+    }
+    else {
+        display.textContent += event.target.textContent;
+    }
+    displayNumber = parseFloat(display.textContent);
+    isOperatorLastClick = false;
+    isEqualsLastClick = false;
+}
+
+function handleOperatorClick(event) {
+    if (calculatorInput.num1 == undefined) {
+        formula.textContent = displayNumber + ' ' + event.target.textContent;
+        calculatorInput.num1 = parseFloat(displayNumber);
+        calculatorInput.operator = event.target.textContent;
+        isOperatorLastClick = true;
+        isEqualsLastClick = false;
+    }
+}
+
+function handleEqualsClick(event) {
+    // For cases of consecutive click to Equals. Don't compute again.
+    if (isEqualsLastClick === true) return;
+
+    calculatorInput.num2 = parseFloat(displayNumber);
+    formula.textContent += ' ' + displayNumber + ' =';
+    displayNumber = operate(calculatorInput.operator, calculatorInput.num1, calculatorInput.num2);
+    display.textContent = displayNumber;
+    isEqualsLastClick = true;
+    isOperatorLastClick = false;
+}
+
+function handleClearClick(event) {
+    calculatorInput.num1 = undefined;
+    calculatorInput.num2 = undefined;
+    calculatorInput.operator = undefined;
+    display.textContent = '0';
+    formula.textContent = '';
+    isEqualsLastClick = false;
 }
 
 let numberButtons = document.querySelectorAll('.btn-number');
 numberButtons.forEach(function(button) {
     button.addEventListener('click', handleNumberClick);
 });
+
+let operatorButtons = document.querySelectorAll('.btn-operator');
+operatorButtons.forEach(function(button) {
+    button.addEventListener('click', handleOperatorClick);
+})
+
+let equalButton = document.querySelector('#equal-operator');
+equalButton.addEventListener('click', handleEqualsClick);
+
+let clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', handleClearClick);
